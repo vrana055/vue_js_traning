764aa3f599115f0d07b03c1d24edac1daff57782
Vibhanshu <vrana055@gmail.com>
2020-10-05 15:35:23 +0530

    on change event

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,14 +1,32 @@
 <template>
-    <p>{{todo.title}}</p>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{todo.title}}
+        </p>
+    </div>
 </template>
 
 <script>
 export default {
     name: "TodoItem",
-    props: ["todo"]
+    props: ["todo"],
+    methods: {
+        markComplete() {
+            console.log(123)
+        }
+    }
 }
 </script>
 
 <style scoped>
+    .todo-item {
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+    }
 
+    .is-complete {
+        text-decoration: line-through;
+    }
 </style>
