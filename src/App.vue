cf323351f0d839829e6eb3c0da0cf6eb1940e1f7
Vibhanshu <vrana055@gmail.com>
2020-10-07 12:11:08 +0530

    addTodo by axios

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
 import Header from "./components/layout/Header";
+import axios from "axios";
 
 export default {
   name: "App",
@@ -20,23 +21,7 @@ export default {
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: false
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        }
-      ]
+      todos: []
     };
   },
   methods: {
@@ -46,6 +31,11 @@ export default {
     addTodo(newTodo) {
        this.todos = [...this.todos, newTodo];
     }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+      .then(res => this.todos = res.data )
+      .catch(err => console.log(err ));
   }
 };
 </script>
